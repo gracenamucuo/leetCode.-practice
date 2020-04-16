ca66d21837e549db4316d6ce35c1ab3366173469
戴运鹏 <dyp@z.vip>
2020-04-16 16:11:20 +0800

    练习插入排序

---
+++ leetCodePractice/Practice.playground/Contents.swift
@@ -0,0 +1,49 @@
+import Cocoa
+///插入排序_交换
+func insertSort_swap(_ array:[Int]) -> [Int] {
+    var a = array
+    for x in 1 ..< a.count {
+        var y = x
+        while y > 0 && a[y] < a[y - 1] {
+            a.swapAt(y, y-1)
+            y -= 1
+        }
+    }
+    return a
+}
+print(insertSort_swap([3,-1,4,8,-2,-6,9,-5,0]))
+
+///插入排序_移动
+func insetSort_shift(array:[Int])->[Int]{
+    var a = array
+    for x in 1 ..< a.count {
+        var y = x
+        let temp = a[y]
+        while y > 0 && temp < a[y - 1] { // 一直用当前遍历的值去比较
+               a[y] = a[y - 1]
+                y -= 1
+        }
+        a[y] = temp
+    }
+    return a
+}
+
+insetSort_shift(array: [3,-1,4,8,-2,-6,9,-5,0])
+
+///泛型
+func insetSort_genericity<T:Comparable>(_ array:[T],_ comeBeforeOrder:(_:T,_:T)->Bool) -> [T]{
+    var a = array
+    for x in 1 ..< a.count {
+        var y = x
+        let temp = a[y]
+        while y > 0 && comeBeforeOrder(temp,a[y - 1]) { // 一直用当前遍历的值去比较
+               a[y] = a[y - 1]
+                y -= 1
+        }
+        a[y] = temp
+    }
+    return a
+}
+print(insetSort_genericity([3,-1,4,8,-2,-6,9,-5,0], >))
+
+
